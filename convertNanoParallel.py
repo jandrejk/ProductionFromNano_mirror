c82b6050249a229edf141cf0825d86ac5cc92f0e
mflechl <martin.flechl@cern.ch>
2018-05-18 12:32:14 +0200

    Updating README Some cleaning

--- convertNanoParallel.py
+++ convertNanoParallel.py
@@ -8,7 +8,6 @@ from ROOT import gSystem, TChain, TSystem, TFile, TString, vector
 
 from PSet import process
 
-#dir = "/data/higgs/nanonaod_2016/PUMoriond17_05Feb2018_94X_mcRun2_asymptotic_v2-v1/VBFHToTauTau_M125_13TeV_powheg_pythia8/"
 channel = sys.argv[1]
 fileNames = [ sys.argv[2] ]
 
@@ -35,21 +34,10 @@ if applyRecoil :
 status = 1
 gSystem.CompileMacro('HTTEvent.cxx','k')
 status *= gSystem.CompileMacro('syncDATA.C','k')
-#status *= gSystem.CompileMacro('NanoEventsSkeleton.C') #RECOMPILE IF IT CHANGES!
 status *= gSystem.CompileMacro('NanoEventsSkeleton.C','k')
 gSystem.Load('$CMSSW_BASE/lib/$SCRAM_ARCH/libTauAnalysisClassicSVfit.so')
 gSystem.Load('$CMSSW_BASE/lib/$SCRAM_ARCH/libTauAnalysisSVfitTF.so')
 gSystem.Load('$CMSSW_BASE/lib/$SCRAM_ARCH/libHTT-utilitiesRecoilCorrections.so')
-
-# xline=gSystem.GetMakeSharedLib()+' -Wattributes'
-# xline=xline.replace(' -W ',' -W -Wattributes ')
-# gSystem.SetMakeSharedLib(xline)
-# print 'MMM ',gSystem.GetMakeSharedLib()
-
-# yline=gSystem.GetMakeExe()+' -Wattributes'
-# yline=yline.replace(' -W ',' -W -Wattributes ')
-# gSystem.SetMakeExe(yline)
-# print 'NNN ',gSystem.GetMakeExe()
 
 stdout = sys.stdout
 sys.stdout = open('/tmp/pstd', 'w')
@@ -89,31 +77,6 @@ for name in fileNames:
     if channel=='mt' or channel=='all': HMuTauhTreeFromNano(  aTree,doSvFit,applyRecoil,vlumis).Loop(nevents,sync_event)
     if channel=='et' or channel=='all': HElTauhTreeFromNano(  aTree,doSvFit,applyRecoil,vlumis).Loop(nevents,sync_event)
     if channel=='tt' or channel=='all': HTauhTauhTreeFromNano(aTree,doSvFit,applyRecoil,vlumis).Loop(nevents,sync_event)
-
-#    print 'A',name,threading.active_count()
-#    t = threading.Thread(target=runFile, args=(aFile,) )
-#    threads += [t]
-#    print 'B',name,threading.active_count()
-#    while threading.active_count()>nthreads:  #pause until thread slots become available                                                                                
-#        pass
-#    print 'C',name,threading.active_count()
-#    t.start()
-
-
-
-
-
-#    print "Making the MuTau tree"
-#    aROOTFile = TFile.Open(aFile)
-#    aTree = aROOTFile.Get("Events")
-#    print "TTree entries: ",aTree.GetEntries()
-#    HMuTauhTreeFromNano(aTree,doSvFit,applyRecoil,vlumis).Loop(nevents,sync_event)
-##    HMuTauhTreeFromNano(aTree,doSvFit,applyRecoil,vlumis).Loop()
-
-#    print "Making the TauTau tree"
-#    aROOTFile = TFile.Open(aFile)
-#    aTree = aROOTFile.Get("Events")
-#    HTauhTauhTreeFromNano(aTree,doSvFit,applyRecoil,vlumis).Loop(nevents)
 
     if nevents>0: break
 
