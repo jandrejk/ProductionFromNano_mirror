c78d85faf6b6cc38d153dc5eefd65121d109947b
Markus Spanring <markusspanring@gmail.com>
2018-06-14 13:13:57 +0200

    batch submit script

---
+++ submit_on_batch.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+#SBATCH -J ZoomZoom 
+#SBATCH -D ${rundir}
+#SBATCH -o ${rundir}/log.txt
+export X509_USER_PROXY='/afs/hephy.at/user/m/mspanring/proxy/x509_proxy'
+echo "---------------------"
+echo "Grid certificate"
+voms-proxy-info --all
+echo "---------------------"
+
+eval `scramv1 runtime -sh`
+./convertNanoParallel.py ${channel} ${file} ${svfit} ${recoil}
+
+mv -f HTT*root ${outdir}
+rm *.h *.cxx *.C *.cc *.py *.pyc *.root *.d *.so *.pcm
