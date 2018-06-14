8f560f88ebfeda7ec46ea2c3d60910ab340a83c1
Markus Spanring <markusspanring@gmail.com>
2018-06-14 13:25:52 +0200

    adapt to new runscript

--- convertNanoParallel.py
+++ convertNanoParallel.py
@@ -4,24 +4,26 @@ import os
 import sys
 import threading
 
-from ROOT import gSystem, TChain, TSystem, TFile, TString, vector
+from ROOT import gSystem, TChain, TSystem, TFile, TString, vector, TFileCollection
 
 from PSet import process
 
 #dir = "/data/higgs/nanonaod_2016/PUMoriond17_05Feb2018_94X_mcRun2_asymptotic_v2-v1/VBFHToTauTau_M125_13TeV_powheg_pythia8/"
-channel = sys.argv[1]
-fileNames = [ sys.argv[2] ]
+channel =          sys.argv[1]
+aFile =            sys.argv[2]
+doSvFit =          int(sys.argv[3])
+applyRecoil=       int(sys.argv[4])
 
+if not "root://" in aFile: aFile = "file://" + aFile
+print sys.argv
 #sync_event=850381
 sync_event=0
 #doSvFit = True
-doSvFit = False
-applyRecoil=True
+
 #applyRecoil=False
 nevents=-1      #all
 #nevents=5000
 vlumis = vector('string')()
-nthreads = 6
 
 print 'Channel: ',channel
 
@@ -29,6 +31,13 @@ if doSvFit :
     print "Run with SVFit computation"
 if applyRecoil :
     print "Apply MET recoil corrections"
+
+print "Using file: ",aFile
+aROOTFile = TFile.Open(aFile)
+aTree = aROOTFile.Get("Events")
+
+print "TTree entries: ",aTree.GetEntries()
+print "Compiling...."
 
 #Some system have problem runnig compilation (missing glibc-static library?).
 #First we try to compile, and only then we start time consuming cmssw
@@ -41,26 +50,11 @@ gSystem.Load('$CMSSW_BASE/lib/$SCRAM_ARCH/libTauAnalysisClassicSVfit.so')
 gSystem.Load('$CMSSW_BASE/lib/$SCRAM_ARCH/libTauAnalysisSVfitTF.so')
 gSystem.Load('$CMSSW_BASE/lib/$SCRAM_ARCH/libHTT-utilitiesRecoilCorrections.so')
 
-# xline=gSystem.GetMakeSharedLib()+' -Wattributes'
-# xline=xline.replace(' -W ',' -W -Wattributes ')
-# gSystem.SetMakeSharedLib(xline)
-# print 'MMM ',gSystem.GetMakeSharedLib()
 
-# yline=gSystem.GetMakeExe()+' -Wattributes'
-# yline=yline.replace(' -W ',' -W -Wattributes ')
-# gSystem.SetMakeExe(yline)
-# print 'NNN ',gSystem.GetMakeExe()
-
-stdout = sys.stdout
-sys.stdout = open('/tmp/pstd', 'w')
-stderr = sys.stderr
-sys.stderr = open('/tmp/perr', 'w')
 status *= gSystem.CompileMacro('HTauTauTreeFromNanoBase.C','k')
 if channel=='mt' or channel=='all': status *= gSystem.CompileMacro('HMuTauhTreeFromNano.C','k')
 if channel=='et' or channel=='all': status *= gSystem.CompileMacro('HElTauhTreeFromNano.C','k')
 if channel=='tt' or channel=='all': status *= gSystem.CompileMacro('HTauhTauhTreeFromNano.C','k')
-sys.stdout=stdout
-sys.stderr=stderr
 
 print "Compilation status: ",status
 if status==0:
@@ -76,45 +70,15 @@ lumisToProcess = process.source.lumisToProcess
 for lumi in lumisToProcess:
     vlumis.push_back(lumi)
 
-threads = []
-ctr=0
-for name in fileNames:
-#    aFile = "file:///home/mbluj/work/data/NanoAOD/80X_with944/VBFHToTauTau_M125_13TeV_powheg_pythia8/RunIISummer16NanoAOD_PUMoriond17_05Feb2018_94X_mcRun2_asymptotic_v2-v1/"+name
-    aFile = "file://"+name
+# for name in fileNames:
 
-    print "Using file: ",aFile
-    aROOTFile = TFile.Open(aFile)
-    aTree = aROOTFile.Get("Events")
-    print "TTree entries: ",aTree.GetEntries()
-    if channel=='mt' or channel=='all': HMuTauhTreeFromNano(  aTree,doSvFit,applyRecoil,vlumis).Loop(nevents,sync_event)
-    if channel=='et' or channel=='all': HElTauhTreeFromNano(  aTree,doSvFit,applyRecoil,vlumis).Loop(nevents,sync_event)
-    if channel=='tt' or channel=='all': HTauhTauhTreeFromNano(aTree,doSvFit,applyRecoil,vlumis).Loop(nevents,sync_event)
-
-#    print 'A',name,threading.active_count()
-#    t = threading.Thread(target=runFile, args=(aFile,) )
-#    threads += [t]
-#    print 'B',name,threading.active_count()
-#    while threading.active_count()>nthreads:  #pause until thread slots become available                                                                                
-#        pass
-#    print 'C',name,threading.active_count()
-#    t.start()
+    # aFile = "file://"+name
 
 
 
+if channel=='mt' or channel=='all': HMuTauhTreeFromNano(  aTree,doSvFit,applyRecoil,vlumis).Loop(nevents,sync_event)
+if channel=='et' or channel=='all': HElTauhTreeFromNano(  aTree,doSvFit,applyRecoil,vlumis).Loop(nevents,sync_event)
+if channel=='tt' or channel=='all': HTauhTauhTreeFromNano(aTree,doSvFit,applyRecoil,vlumis).Loop(nevents,sync_event)
 
-
-#    print "Making the MuTau tree"
-#    aROOTFile = TFile.Open(aFile)
-#    aTree = aROOTFile.Get("Events")
-#    print "TTree entries: ",aTree.GetEntries()
-#    HMuTauhTreeFromNano(aTree,doSvFit,applyRecoil,vlumis).Loop(nevents,sync_event)
-##    HMuTauhTreeFromNano(aTree,doSvFit,applyRecoil,vlumis).Loop()
-
-#    print "Making the TauTau tree"
-#    aROOTFile = TFile.Open(aFile)
-#    aTree = aROOTFile.Get("Events")
-#    HTauhTauhTreeFromNano(aTree,doSvFit,applyRecoil,vlumis).Loop(nevents)
-
-    if nevents>0: break
 
 exit(0)
