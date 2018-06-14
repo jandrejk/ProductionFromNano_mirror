5168a58702d2879c4bf65a8d86b28f1071d21c65
Markus Spanring <markusspanring@gmail.com>
2018-06-14 13:18:48 +0200

    removing getMT calculated on the fly in header; adding getMTTOT and getPTTOT; TES now applied again in HTTParticle; minor restructuring

--- HTTEvent.cxx
+++ HTTEvent.cxx
@@ -91,213 +91,244 @@ void HTTParticle::clear(){
 }
 ////////////////////////////////////////////////
 ////////////////////////////////////////////////
-const TLorentzVector & HTTParticle::getNominalShiftedP4() const{
+const TLorentzVector & HTTParticle::getNominalShiftedP4() const
+{
 
-  return p4;
+    p4Cache = p4;
 
-  //Corrections of nominal tau-scale: https://twiki.cern.ch/twiki/bin/view/CMS/TauIDRecommendation13TeV#Tau_energy_scale
+    if( std::abs(getPDGid())==15 )
+    {
+        int dm = getProperty(PropertyEnum::decayMode);
+        float tauES;
 
-  //Done in a different place now
-  /*
-  //correct nominal scale of genuine taus
-  if(std::abs(getPDGid())==15 && getProperty(PropertyEnum::mc_match)==5){
-    int dm = getProperty(PropertyEnum::decayMode);
-    if(dm==0) //1-prong
-      return getShiftedP4(1+TES_1p,true);
-    else if(dm==1 || dm==2) //1-prong+pi0s
-      return getShiftedP4(1+TES_1ppi0,false);
-    else if(dm==10) //3-prongs
-      return getShiftedP4(1+TES_3p,false);
-    else //others
-      return p4;
+        if( getProperty(PropertyEnum::mc_match)==5 )
+        {
+            if(dm == 0 ) tauES = 0.0;
+            if(dm == 1 ) tauES = 0.0;
+            if(dm == 10) tauES = 0.0;
+        }
+        if( getProperty(PropertyEnum::mc_match)==1 )
+        {
+            if(dm == 0 ) tauES = 0.0;
+            if(dm == 1 ) tauES = 0.0;
+            if(dm == 10) tauES = 0.0;
+        }
+        if( getProperty(PropertyEnum::mc_match)==2 )
+        {
+            if(dm == 0 ) tauES = 0.0;
+            if(dm == 1 ) tauES = 0.0;
+            if(dm == 10) tauES = 0.0;
+        }
+
+        float tauES_mass = tauES;
+        if (dm == 0) tauES_mass=0;
+
+        p4Cache.SetPtEtaPhiM(p4.Pt() * (1.0+tauES),
+                             p4.Eta(),
+                             p4.Phi(),
+                             p4.M() * (1.0+tauES_mass) );
+
     }
-    else return p4;
-  */
-}
-////////////////////////////////////////////////
-////////////////////////////////////////////////
-const TLorentzVector & HTTParticle::getSystScaleP4(HTTAnalysis::sysEffects type) const{
 
-  if(type==HTTAnalysis::DUMMY_SYS) {
-    lastSystEffect = type;
-    return p4;
-  }
-  else if(type==HTTAnalysis::NOMINAL){
-    lastSystEffect = type;
-    return getNominalShiftedP4();
-  }
-  else if(lastSystEffect==type) return p4Cache;
-
-  lastSystEffect = type;
-
-  if(std::abs(getPDGid())==15 && getProperty(PropertyEnum::mc_match)==5){
-    ///True taus
-    if(type!=HTTAnalysis::TESUp && type!=HTTAnalysis::TESDown) return getNominalShiftedP4();
-    float direction = 1;
-    if(type==HTTAnalysis::TESDown) direction*=-1;
-    float nominalShift = 1.0;
-    int dm = getProperty(PropertyEnum::decayMode);
-    if(dm==0) nominalShift = 1+TES_1p;
-    else if(dm==1 || dm==2) nominalShift = 1+TES_1ppi0;
-    else if(dm==10) nominalShift = 1+TES_3p;
-    float shift = nominalShift*(1+direction*TES);
-    return getShiftedP4(shift,getProperty(PropertyEnum::decayMode)==0);
-  }
-  if(std::abs(getPDGid())==15 && getProperty(PropertyEnum::mc_match)==3){
-    ///Fake e->tau
-    if(type!=HTTAnalysis::E2TUp && type!=HTTAnalysis::E2TDown) return p4;
-    float direction = 1;
-    if(type==HTTAnalysis::E2TDown) direction*=-1;
-    return getShiftedP4(1+direction*EES,getProperty(PropertyEnum::decayMode)==0);
-  }
-  if(std::abs(getPDGid())==15 && getProperty(PropertyEnum::mc_match)==4){
-    ///Fake mu->tau
-    if(type!=HTTAnalysis::M2TUp && type!=HTTAnalysis::M2TDown) return p4;
-    float direction = 1;
-    if(type==HTTAnalysis::M2TDown) direction*=-1;
-    return getShiftedP4(1+direction*MES,getProperty(PropertyEnum::decayMode)==0);
-  }
-  if(std::abs(getPDGid())==98){
-    if(type!=HTTAnalysis::JESUp && type!=HTTAnalysis::JESDown) return p4;
-    float JES = 0;
-    switch((int)type) {
-    case (int)HTTAnalysis::JESUp :
-      JES = getProperty(PropertyEnum((int)PropertyEnum::NONE+(int)JecUncEnum::Total));
-      break;
-    case (int)HTTAnalysis::JESDown :
-      //JES = -getProperty(PropertyEnum((int)PropertyEnum::NONE+(int)JecUncEnum::NONE+(int)JecUncEnum::Total));//Down uncertainties always with "-"
-      JES = -getProperty(PropertyEnum((int)PropertyEnum::NONE+(int)JecUncEnum::Total));//For "Total" use symmetrically Up uncert also for Down (correct?)
-      break;
-    }
-    return getShiftedP4(1+JES,false);
-  }
-
-  p4Cache = p4;
-  return p4;
-}
-////////////////////////////////////////////////
-////////////////////////////////////////////////
-const TLorentzVector & HTTParticle::getShiftedP4(float scale, bool preserveMass) const{
-
-  if(!preserveMass){
-    p4Cache = scale*p4;
     return p4Cache;
-  }
-
-  double pt = p4.Perp();
-  double energy =  p4.E();
-  pt*=scale;
-  double shiftedMomentum = pt/sin(p4.Theta());
-  energy = sqrt(p4.M2() + pow(shiftedMomentum,2));
-  p4Cache = p4;
-  p4Cache.SetRho(shiftedMomentum);
-  p4Cache.SetE(energy);
-  return p4Cache;
 }
 ////////////////////////////////////////////////
 ////////////////////////////////////////////////
-void HTTPair::clear(){
+const TLorentzVector & HTTParticle::getSystScaleP4(HTTAnalysis::sysEffects type) const
+{
 
-  if(!p4Vector.size()) p4Vector.resize(HTTAnalysis::DUMMY_SYS);
-  if(!leg1p4Vector.size()) leg1p4Vector.resize(HTTAnalysis::DUMMY_SYS);
-  if(!leg2p4Vector.size()) leg2p4Vector.resize(HTTAnalysis::DUMMY_SYS);
-  if(!svMetVector.size()) svMetVector.resize(HTTAnalysis::DUMMY_SYS);
+    if(type==HTTAnalysis::DUMMY_SYS)
+    {
+      lastSystEffect = type;
+      return p4;
 
-  for(auto &it:p4Vector) it*=0;
-  for(auto &it:leg1p4Vector) it*=0;
-  for(auto &it:leg2p4Vector) it*=0;
-  for(auto &it:svMetVector) it*=0;
-
-  metMatrix.clear();
-
-  mtLeg1= -999;
-  mtLeg2 = -999;
-
-  leg1.clear();
-  leg2.clear();
-
-  lastSystEffect = HTTAnalysis::NOMINAL;
-}
-////////////////////////////////////////////////
-////////////////////////////////////////////////
-const TLorentzVector & HTTPair::getP4(HTTAnalysis::sysEffects type) const {
-
-  if(p4Vector.size()>(unsigned int)type) return p4Vector[(unsigned int)type];
-  return p4Vector[(unsigned int)HTTAnalysis::NOMINAL];
-}
-////////////////////////////////////////////////
-////////////////////////////////////////////////
-const TLorentzVector & HTTPair::getLeg1P4(HTTAnalysis::sysEffects type) const {
-
-  if(leg1p4Vector.size()>(unsigned int)type) return leg1p4Vector[(unsigned int)type];
-  return leg1p4Vector[(unsigned int)HTTAnalysis::NOMINAL];
-}
-////////////////////////////////////////////////
-////////////////////////////////////////////////
-const TLorentzVector & HTTPair::getLeg2P4(HTTAnalysis::sysEffects type) const {
-
-  if(leg2p4Vector.size()>(unsigned int)type) return leg2p4Vector[(unsigned int)type];
-  return leg2p4Vector[(unsigned int)HTTAnalysis::NOMINAL];
-}
-////////////////////////////////////////////////
-////////////////////////////////////////////////
-const TVector2 & HTTPair::getSystScaleMET(HTTAnalysis::sysEffects type) const{
-
-  if(type==HTTAnalysis::NOMINAL ||
-  (unsigned int)type>(unsigned int)HTTAnalysis::DUMMY_SYS) {
-    lastSystEffect = type;
-    if( (std::abs(leg1.getPDGid())==15 && leg1.getProperty(PropertyEnum::mc_match)==5) ||
-	(std::abs(leg2.getPDGid())==15 && leg2.getProperty(PropertyEnum::mc_match)==5) ){
-
-      double metX = met.X();
-      metX+=leg1.getP4(HTTAnalysis::DUMMY_SYS).X(); //uncor
-      metX+=leg2.getP4(HTTAnalysis::DUMMY_SYS).X(); //uncor
-      metX-=leg1.getP4(HTTAnalysis::NOMINAL).X();
-      metX-=leg2.getP4(HTTAnalysis::NOMINAL).X();
-
-      double metY = met.Y();
-      metY+=leg1.getP4(HTTAnalysis::DUMMY_SYS).Y();
-      metY+=leg2.getP4(HTTAnalysis::DUMMY_SYS).Y();
-      metY-=leg1.getP4(HTTAnalysis::NOMINAL).Y();
-      metY-=leg2.getP4(HTTAnalysis::NOMINAL).Y();
-
-      metCache.SetX(metX);
-      metCache.SetY(metY);
-      return metCache;
+    }else if(type==HTTAnalysis::NOMINAL){
+      lastSystEffect = type;
+      return getNominalShiftedP4();
     }
-    return met;
-  }
-  else if(lastSystEffect==type) return metCache;
+    else if(lastSystEffect==type) return p4Cache;
 
-  double metX = met.X();
-  metX+=leg1.getP4(HTTAnalysis::DUMMY_SYS).X();
-  metX+=leg2.getP4(HTTAnalysis::DUMMY_SYS).X();
-  metX-=leg1.getP4(type).X();
-  metX-=leg2.getP4(type).X();
+    lastSystEffect = type;
 
-  double metY = met.Y();
-  metY+=leg1.getP4(HTTAnalysis::DUMMY_SYS).Y();
-  metY+=leg2.getP4(HTTAnalysis::DUMMY_SYS).Y();
-  metY-=leg1.getP4(type).Y();
-  metY-=leg2.getP4(type).Y();
+    // if(std::abs(getPDGid())==15 && getProperty(PropertyEnum::mc_match)==5){
+    //   ///True taus
+    //   if(type!=HTTAnalysis::TESUp && type!=HTTAnalysis::TESDown) return getNominalShiftedP4();
+    //   float direction = 1;
+    //   if(type==HTTAnalysis::TESDown) direction*=-1;
+    //   float nominalShift = 1.0;
+    //   int dm = getProperty(PropertyEnum::decayMode);
+    //   if(dm==0) nominalShift = 1+TES_1p;
+    //   else if(dm==1 || dm==2) nominalShift = 1+TES_1ppi0;
+    //   else if(dm==10) nominalShift = 1+TES_3p;
+    //   float shift = nominalShift*(1+direction*TES);
+    //   return getShiftedP4(shift,getProperty(PropertyEnum::decayMode)==0);
+    // }
+    // if(std::abs(getPDGid())==15 && getProperty(PropertyEnum::mc_match)==3){
+    //   ///Fake e->tau
+    //   if(type!=HTTAnalysis::E2TUp && type!=HTTAnalysis::E2TDown) return p4;
+    //   float direction = 1;
+    //   if(type==HTTAnalysis::E2TDown) direction*=-1;
+    //   return getShiftedP4(1+direction*EES,getProperty(PropertyEnum::decayMode)==0);
+    // }
+    // if(std::abs(getPDGid())==15 && getProperty(PropertyEnum::mc_match)==4){
+    //   ///Fake mu->tau
+    //   if(type!=HTTAnalysis::M2TUp && type!=HTTAnalysis::M2TDown) return p4;
+    //   float direction = 1;
+    //   if(type==HTTAnalysis::M2TDown) direction*=-1;
+    //   return getShiftedP4(1+direction*MES,getProperty(PropertyEnum::decayMode)==0);
+    // }
+    // if(std::abs(getPDGid())==98){
+    //   if(type!=HTTAnalysis::JESUp && type!=HTTAnalysis::JESDown) return p4;
+    //   float JES = 0;
+    //   switch((int)type) {
+    //   case (int)HTTAnalysis::JESUp :
+    //     JES = getProperty(PropertyEnum((int)PropertyEnum::NONE+(int)JecUncEnum::Total));
+    //     break;
+    //   case (int)HTTAnalysis::JESDown :
+    //     //JES = -getProperty(PropertyEnum((int)PropertyEnum::NONE+(int)JecUncEnum::NONE+(int)JecUncEnum::Total));//Down uncertainties always with "-"
+    //     JES = -getProperty(PropertyEnum((int)PropertyEnum::NONE+(int)JecUncEnum::Total));//For "Total" use symmetrically Up uncert also for Down (correct?)
+    //     break;
+    //   }
+    //   return getShiftedP4(1+JES,false);
+    // }
 
-  metCache.SetX(metX);
-  metCache.SetY(metY);
+    p4Cache = p4;
 
-  return metCache;
+    return p4;
 }
 ////////////////////////////////////////////////
 ////////////////////////////////////////////////
-float HTTPair::getSystScaleMT(const HTTParticle &aParticle,
-			      HTTAnalysis::sysEffects type) const{
+const TLorentzVector & HTTParticle::getShiftedP4(float scale, bool preserveMass) const
+{
 
-  const TVector2 & metScaled = getSystScaleMET(type);
-  const TLorentzVector & legP4 = aParticle.getP4(type);
-  float sumP2 = pow(metScaled.X() + legP4.X(),2) +
-                pow(metScaled.Y() + legP4.Y(),2);
-  float sumE2 = pow(metScaled.Mod() + legP4.Perp(),2);
-  float mT = sqrt(sumE2 - sumP2);
-  return mT;
+    if(!preserveMass){
+        p4Cache = scale*p4;
+        return p4Cache;
+    }
+
+    double pt = p4.Perp();
+    double energy =  p4.E();
+    pt*=scale;
+    double shiftedMomentum = pt/sin(p4.Theta());
+    energy = sqrt(p4.M2() + pow(shiftedMomentum,2));
+    p4Cache = p4;
+    p4Cache.SetRho(shiftedMomentum);
+    p4Cache.SetE(energy);
+    return p4Cache;
+}
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+void HTTPair::clear()
+{
+
+    if(!p4Vector.size()) p4Vector.resize(HTTAnalysis::DUMMY_SYS);
+    if(!leg1p4Vector.size()) leg1p4Vector.resize(HTTAnalysis::DUMMY_SYS);
+    if(!leg2p4Vector.size()) leg2p4Vector.resize(HTTAnalysis::DUMMY_SYS);
+    if(!svMetVector.size()) svMetVector.resize(HTTAnalysis::DUMMY_SYS);
+
+    for(auto &it:p4Vector) it*=0;
+    for(auto &it:leg1p4Vector) it*=0;
+    for(auto &it:leg2p4Vector) it*=0;
+    for(auto &it:svMetVector) it*=0;
+
+    metMatrix.clear();
+
+    // mtLeg1= -999;
+    // mtLeg2 = -999;
+
+    leg1.clear();
+    leg2.clear();
+
+    lastSystEffect = HTTAnalysis::NOMINAL;
+}
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+const TLorentzVector & HTTPair::getP4(HTTAnalysis::sysEffects type) const
+{
+    if(p4Vector.size()>(unsigned int)type) return p4Vector[(unsigned int)type];
+    return p4Vector[(unsigned int)HTTAnalysis::NOMINAL];
+}
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+const TLorentzVector & HTTPair::getLeg1P4(HTTAnalysis::sysEffects type) const
+{
+    if(leg1p4Vector.size()>(unsigned int)type) return leg1p4Vector[(unsigned int)type];
+    return leg1p4Vector[(unsigned int)HTTAnalysis::NOMINAL];
+}
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+const TLorentzVector & HTTPair::getLeg2P4(HTTAnalysis::sysEffects type) const
+{
+    if(leg2p4Vector.size()>(unsigned int)type) return leg2p4Vector[(unsigned int)type];
+    return leg2p4Vector[(unsigned int)HTTAnalysis::NOMINAL];
+}
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+const TVector2 & HTTPair::getSystScaleMET(HTTAnalysis::sysEffects type) const
+{
+
+    if(type==HTTAnalysis::NOMINAL
+      || (unsigned int)type>(unsigned int)HTTAnalysis::DUMMY_SYS)
+    {
+      
+        lastSystEffect = type;
+        if( (std::abs(leg1.getPDGid())==15 && leg1.getProperty(PropertyEnum::mc_match)==5) 
+            || (std::abs(leg2.getPDGid())==15 && leg2.getProperty(PropertyEnum::mc_match)==5) )
+        {
+
+            double metX = met.X();
+            metX+=leg1.getP4(HTTAnalysis::DUMMY_SYS).X(); //uncor
+            metX+=leg2.getP4(HTTAnalysis::DUMMY_SYS).X(); //uncor
+            metX-=leg1.getP4(HTTAnalysis::NOMINAL).X();
+            metX-=leg2.getP4(HTTAnalysis::NOMINAL).X();
+
+            double metY = met.Y();
+            metY+=leg1.getP4(HTTAnalysis::DUMMY_SYS).Y();
+            metY+=leg2.getP4(HTTAnalysis::DUMMY_SYS).Y();
+            metY-=leg1.getP4(HTTAnalysis::NOMINAL).Y();
+            metY-=leg2.getP4(HTTAnalysis::NOMINAL).Y();
+
+            metCache.SetX(metX);
+            metCache.SetY(metY);
+            return metCache;
+        }
+        return met;
+    }
+    else if(lastSystEffect==type) return metCache;
+
+    double metX = met.X();
+    metX+=leg1.getP4(HTTAnalysis::DUMMY_SYS).X();
+    metX+=leg2.getP4(HTTAnalysis::DUMMY_SYS).X();
+    metX-=leg1.getP4(type).X();
+    metX-=leg2.getP4(type).X();
+
+    double metY = met.Y();
+    metY+=leg1.getP4(HTTAnalysis::DUMMY_SYS).Y();
+    metY+=leg2.getP4(HTTAnalysis::DUMMY_SYS).Y();
+    metY-=leg1.getP4(type).Y();
+    metY-=leg2.getP4(type).Y();
+
+    metCache.SetX(metX);
+    metCache.SetY(metY);
+
+    return metCache;
+}
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+float HTTPair::getMTTOT(HTTAnalysis::sysEffects type) const
+{
+  float mt1 = 2. * leg1.getP4(type).Pt() * getSystScaleMET(type).Mod() * (1. - TMath::Cos(leg1.getP4(type).Phi()-getSystScaleMET(type).Phi()));
+  float mt2 = 2. * leg2.getP4(type).Pt() * getSystScaleMET(type).Mod() * (1. - TMath::Cos(leg2.getP4(type).Phi()-getSystScaleMET(type).Phi()));
+  float mt3 = 2. * leg1.getP4(type).Pt() * leg2.getP4(type).Pt() * (1. - TMath::Cos(leg1.getP4(type).Phi()-leg2.getP4(type).Phi()));
+  return TMath::Sqrt( mt1 + mt2 + mt3 );
+}
+
+float HTTPair::getPTTOT(HTTAnalysis::sysEffects type) const
+{
+  TLorentzVector vmet; 
+  vmet.SetPtEtaPhiM(getSystScaleMET(type).Mod(),0,getSystScaleMET(type).Phi(),0);
+
+  return (vmet + leg1.getP4(type) + leg2.getP4(type) ).Pt();
 }
 ////////////////////////////////////////////////
 ////////////////////////////////////////////////
