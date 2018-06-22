af7305fdf6bf635d4061efef063ab8b1c79088ea
Markus Spanring <markusspanring@gmail.com>
2018-06-22 14:23:49 +0200

    config file for energy scales

---
+++ EnergyScales.h
@@ -0,0 +1,22 @@
+const struct ES{
+	struct Electron{
+		float oneProng0p0 = 0.0;
+		float oneProng1p0 = 0.0;
+		float threeProng0p0 = 0.0;
+		float uncertaintyShift = 0.03;
+	} Electron;
+
+	struct Muon{
+		float oneProng0p0 = 0.0;
+		float oneProng1p0 = 0.0;
+		float threeProng0p0 = 0.0;
+		float uncertaintyShift = 0.03;
+	} Muon;
+
+	struct Tau{
+		float oneProng0p0 = 0.0;
+		float oneProng1p0 = 0.0;
+		float threeProng0p0 = 0.0;
+		float uncertaintyShift = 0.03;
+	} Tau;
+} ES;
