b8192803adb0c3d1eefaf76f657bd952db8608a4
Markus Spanring <markusspanring@gmail.com>
2018-06-25 17:06:24 +0200

    add systematic shifts

--- run.py
+++ run.py
@@ -17,6 +17,7 @@ def main():
     parser.add_argument('-s', dest='sample', help='Sample to run over', type=str, metavar = 'SAMPLE')
     parser.add_argument('-v', dest='version', help='Version of ntuples.', type=str, metavar = 'VERSION', default = 'v1')
     parser.add_argument('-c', dest='channel', help='Dataset channel',choices = ['mt','et','tt'], default = 'mt')
+    parser.add_argument('-e', dest='shift', help='Uncert shift of energy scale',choices = ['t0u','t1u','t10u','t0d','t1d','t10d','m0u','m1u','m10u','m0d','m1d','m10d','e0u','e1u','e10u','e0d','e1d','e10d'], default = '')
     parser.add_argument('-j', dest='jobs', help='If set to NJOBS > 0: Run NJOBS in parallel on heplx. Otherwise submit to batch.', type=int, default = 0)
     parser.add_argument('-d', dest='debug', help='Debug', action = "store_true")
 
@@ -27,12 +28,12 @@ def main():
     #2017 sync
     # sample = 'VBFHToTauTau_M125_13TeV_powheg_pythia8'
 
-    SNP = SteerNanoProduction(args.channel, args.jobs, args.debug)
+    SNP = SteerNanoProduction(args.channel, args.shift, args.jobs, args.debug)
     SNP.runOneSample(args.sample)
 
 class SteerNanoProduction():
 
-    def __init__(self, channel, nthreads=6, debug=False):
+    def __init__(self, channel, shift, nthreads=6, debug=False):
 
         self.basedir = os.getcwd()
         self.outdir = '/afs/hephy.at/data/higgs01'
@@ -48,6 +49,32 @@ class SteerNanoProduction():
         else:
             self.nthreads = nthreads
             self.nevents = -1
+
+
+        shifts={'t0u' : 'TES1p0p0Up',
+                't1u' : 'TES1p1p0Up',
+                't10u': 'TES3p0p0Up',
+                't0d' : 'TES1p0p0Down',
+                't1d' : 'TES1p1p0Down',
+                't10d': 'TES3p0p0Down',
+                'm0u' : 'MES1p0p0Up',
+                'm1u' : 'MES1p1p0Up',
+                'm10u': 'MES3p0p0Up',
+                'm0d' : 'MES1p0p0Down',
+                'm1d' : 'MES1p1p0Down',
+                'm10d': 'MES3p0p0Down',
+                'e0u' : 'EES1p0p0Up',
+                'e1u' : 'EES1p1p0Up',
+                'e10u': 'EES3p0p0Up',
+                'e0d' : 'EES1p0p0Down',
+                'e1d' : 'EES1p1p0Down',
+                'e10d': 'EES3p0p0Down'
+        }
+
+        if shift:
+            self.systShift = shifts.get(shift,'NOMINAL')
+        else:
+            self.systShift = "NOMINAL"
 
     def runOneSample(self, sample, version="v1"):
         threads = []
@@ -68,7 +95,11 @@ class SteerNanoProduction():
 
             rundir = runpath+str(idx)
 
+            ##### Create rundir. Overwrite if it already exists
             if not os.path.exists(rundir):
+                os.makedirs(rundir)
+            else:
+                shutil.rmtree(rundir)
                 os.makedirs(rundir)
 
             os.chdir(self.basedir)
@@ -89,6 +120,7 @@ class SteerNanoProduction():
                                              outdir = outdir,
                                              file = file,
                                              channel = self.channel,
+                                             systShift = self.systShift,
                                              svfit = int(self.svfit),
                                              recoil = int(self.recoil),
                                              nevents = int(self.nevents) )
@@ -105,7 +137,7 @@ class SteerNanoProduction():
 
             if not self.debug:
                 os.chdir( "/".join([ self.basedir, "out" ,sample ]) )
-                os.system('hadd -f -O '+'ntuple_{0}.root rundir_{0}_*/HTT{1}*root'.format(self.channel, self.channel.upper() ) )
+                os.system('hadd -f -O '+'ntuple_{0}.root rundir_{0}_*/{1}_*root'.format(self.channel, "-".join([self.channel, self.systShift]) ) )
 
     def runOneFileLocal(self, rundir ,file):
 
@@ -116,8 +148,9 @@ class SteerNanoProduction():
 
         print "\033[93mrun\033[0m  Job {0}: ".format(njob) + file.split("/")[-1]
 
-        runcmd  = './convertNanoParallel.py {0} {1} {2} {3} {4}'.format( file,
-                                                                         self.channel,                                                                   
+        runcmd  = './convertNanoParallel.py {0} {1} {2} {3} {4} {5}'.format( file,
+                                                                         self.channel,
+                                                                         self.systShift,
                                                                          int(self.svfit),
                                                                          int(self.recoil),
                                                                          int(self.nevents) )
