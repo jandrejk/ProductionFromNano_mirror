185726ba33be8ec26970f258a0dd8ec627abe21a
Markus Spanring <markusspanring@gmail.com>
2018-07-12 17:58:20 +0200

    use dasgoclient to get filelists

--- getFilelists.py
+++ getFilelists.py
@@ -3,32 +3,10 @@ import shlex
 import json
 import os 
 
+
 def main():
-
-    if not os.path.exists("samples"):
-        os.makedirs("samples")
-
-    print "Searching on DPM"
-    samples = buildPathToSamples()
-
-    for sample in samples:
-
-        if not os.path.exists( "/".join( ["samples"] + sample.split("/")[:-1] ) ):
-            os.makedirs( "/".join( ["samples"] + sample.split("/")[:-1] ) )
-
-        if not os.path.exists( "samples/{0}.txt".format(sample) ):
-            with open( "samples/{0}.txt".format(sample), "w" ) as FSO:
-                FSO.write( getFilelist(samples[sample]) )
-                FSO.write("\n")
-
-def buildPathToSamples():
-    with open("samples_config.json","r") as FSO:
+    with open("sample_collection.json","r") as FSO:
         config = json.load(FSO)
-
-    sampleFormat = {"mc":"NANOAODSIM","data":"NANOAOD"}
-    dpm_path = "/dpm/oeaw.ac.at/home/cms/store/"
-
-    sample_collection= {}
 
     for frmt in config:
         f = config[frmt]
@@ -37,37 +15,37 @@ def buildPathToSamples():
             r = f["run"][run]
 
             for sample in r["samples"]:
-                parts = r["samples"][sample]
+                links = r["samples"][sample]
 
-                for part in parts:
-                    path = "/".join([ dpm_path, frmt, run, part, sampleFormat[frmt] ])
-                    out = lsDPM(path)
-                    for tag in out:
+                for link in links:
+                    folder = "/".join(["samples",frmt, sample])
+                    filename = buildFileName( link, run, r["creation"] )
+                    content = getDASquery(link)
 
-                        if r["creation"] in tag:
-                            sample_collection[ "/".join([frmt, sample, "_".join([part, run, r["creation"] ]) ])  ] = "/".join([ path, tag ])
+                    if "Vienna" in getDASquery(link,"site"):
+                        content = content.replace("/store/","root://hephyse.oeaw.ac.at//store/")
+                    else:
+                        content = content.replace("/store/","root://xrootd-cms.infn.it//store/")
 
-    return sample_collection
-
-def getFilelist(sample):
-
-    filelist = []
-    for folder in lsDPM(sample):
-        path = "/".join([ sample, folder ])
-        files = lsDPM( path )
-
-        for file in files:
-            filelist.append( "/".join([ "root://hephyse.oeaw.ac.at/", path, file ]) )
-
-    return "\n".join(filelist)
+                    writeFile(content, filename, folder)
+                    # print getDASquery(link,"file")
 
 
+def getDASquery(link, info="file"):
 
-def lsDPM(path):
-    proc = sp.Popen(shlex.split("dpns-ls {0}".format(path) ), stdout=sp.PIPE, stderr=sp.PIPE, shell=False)
+    proc = sp.Popen(shlex.split('dasgoclient --query="{0} dataset={1}" '.format(info,link) ), stdout=sp.PIPE, stderr=sp.PIPE, shell=False)
     (out, err) = proc.communicate()
 
-    return out.splitlines()
+    return out
+
+def writeFile( content, filename, folder ):
+    if not os.path.exists(folder):
+        os.mkdirs(folder)
+
+    with open("/".join([folder,filename]), "w" ) as FSO:
+        FSO.write(content)
+
+
 
 
 
