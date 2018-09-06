6756f6b56d19bbfb3cd9bb1b18b800b70979d9a9
Markus Spanring <markusspanring@gmail.com>
2018-09-06 13:49:10 +0200

    switch to TES measured by Izaac

--- EnergyScales.h
+++ EnergyScales.h
@@ -14,9 +14,9 @@ const struct ES{
 	} Muon;
 
 	struct Tau{
-		float oneProng0p0 = 0.0;
-		float oneProng1p0 = 0.0;
-		float threeProng0p0 = 0.0;
+		float oneProng0p0 =   0.008;
+		float oneProng1p0 =  -0.008;
+		float threeProng0p0 = 0.010;
 		float uncertaintyShift = 0.03;
 	} Tau;
 } ES;
