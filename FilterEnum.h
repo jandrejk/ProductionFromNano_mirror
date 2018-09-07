81518fe5953c0a0114ed55649b77af2aa6bb7cb5
Markus Spanring <markusspanring@gmail.com>
2018-09-07 12:29:58 +0200

    moving branch names to header file

--- FilterEnum.h
+++ FilterEnum.h
@@ -1,12 +1,27 @@
 enum class FilterEnum { 
 Flag_goodVertices = 0,
-Flag_globalTightHalo2016Filter = 1,
-Flag_HBHENoiseFilter = 2,
-Flag_HBHENoiseIsoFilter = 3,
-Flag_EcalDeadCellTriggerPrimitiveFilter = 4,
-Flag_BadPFMuonFilter = 5,
-Flag_BadChargedCandidateFilter = 6,
-Flag_eeBadScFilter = 7,
-Flag_ecalBadCalibFilter = 8,
-NONE = 9
+Flag_globalTightHalo2016Filter,
+Flag_HBHENoiseFilter,
+Flag_HBHENoiseIsoFilter,
+Flag_EcalDeadCellTriggerPrimitiveFilter,
+Flag_BadPFMuonFilter,
+Flag_BadChargedCandidateFilter,
+Flag_eeBadScFilter,
+Flag_ecalBadCalibFilter,
+Flag_METFilters,
+NONE
+};
+
+const vector<string> FilterNames =
+{
+"Flag_goodVertices",
+"Flag_globalTightHalo2016Filter",
+"Flag_HBHENoiseFilter",
+"Flag_HBHENoiseIsoFilter",
+"Flag_EcalDeadCellTriggerPrimitiveFilter",
+"Flag_BadPFMuonFilter",
+"Flag_BadChargedCandidateFilter",
+"Flag_eeBadScFilter",
+"Flag_ecalBadCalibFilter",
+"Flag_METFilters"
 };
