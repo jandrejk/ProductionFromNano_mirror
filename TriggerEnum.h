4941de74d7070626ebc2110cb373f3f2daa0a06b
Markus Spanring <markusspanring@gmail.com>
2018-09-07 12:28:31 +0200

    adding etau X-trigger

--- TriggerEnum.h
+++ TriggerEnum.h
@@ -1,10 +1,13 @@
-enum class TriggerEnum { HLT_IsoMu24 = 0, 
-HLT_IsoMu27 = 1, 
-HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1 = 2, 
-HLT_Ele32_WPTight_Gsf = 3, 
-HLT_Ele35_WPTight_Gsf = 4, 
-HLT_DoubleTightChargedIsoPFTau35_Trk1_TightID_eta2p1_Reg = 5, 
-HLT_DoubleMediumChargedIsoPFTau40_Trk1_TightID_eta2p1_Reg = 6, 
-HLT_DoubleTightChargedIsoPFTau40_Trk1_eta2p1_Reg = 7, 
-NONE = 8
+enum class TriggerEnum {
+HLT_IsoMu24 = 0,
+HLT_IsoMu27,
+HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1,
+HLT_Ele32_WPTight_Gsf,
+HLT_Ele35_WPTight_Gsf,
+HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTau30_eta2p1_CrossL1,
+HLT_MediumChargedIsoPFTau180HighPtRelaxedIso_Trk50_eta2p1,
+HLT_DoubleTightChargedIsoPFTau35_Trk1_TightID_eta2p1_Reg,
+HLT_DoubleMediumChargedIsoPFTau40_Trk1_TightID_eta2p1_Reg,
+HLT_DoubleTightChargedIsoPFTau40_Trk1_eta2p1_Reg,
+NONE
 };
