8888ead880cc9a15ef880e2557ba3084213f4d05
Markus Spanring <markusspanring@gmail.com>
2018-11-13 14:59:13 +0100

    Updating ele->tau ES to values measured by Olena

--- EnergyScales.h
+++ EnergyScales.h
@@ -1,10 +1,10 @@
 const struct ES{
 	struct Electron{
-		float oneProng0p0 = 0.01;
-		float oneProng1p0 = 0.041;
+		float oneProng0p0 = 0.003;
+		float oneProng1p0 = 0.036;
 		float threeProng0p0 = 0.0;
-		float uncertaintyShift1p   = 0.014;
-		float uncertaintyShift1p1p = 0.018;
+		float uncertaintyShift1p   = 0.007;
+		float uncertaintyShift1p1p = 0.007;
 		float uncertaintyShift3p   = 0.03;
 	} Electron;
 
